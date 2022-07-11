280e6b5064b5af96aa7841918339e5d9d9a5aad4
Richard Soares <soaresr870@gmail.com>
2022-07-11 20:36:40 -0300

    create project in ASP.NET Core MVC

---
+++ ConsumerFoodSRS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ConsumerFoodSRS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
