177c5bbb527ad9b32d567e3dc29a101b097c1263
Richard Soares <soaresr870@gmail.com>
2022-07-12 18:08:48 -0300

    created controller and view to lanches

---
+++ ConsumerFoodSRS/Controllers/LancheController.cs
@@ -0,0 +1,20 @@
+﻿using ConsumerFoodSRS.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConsumerFoodSRS.Controllers;
+
+public class LancheController : Controller
+{
+    private readonly ILancheRepository _lancheRepository;
+
+    public LancheController(ILancheRepository lancheRepository)
+    {
+        _lancheRepository = lancheRepository;
+    }
+
+    public IActionResult List()
+    {
+        var lanches = _lancheRepository.Lanches;
+        return View(lanches);
+    }
+}
