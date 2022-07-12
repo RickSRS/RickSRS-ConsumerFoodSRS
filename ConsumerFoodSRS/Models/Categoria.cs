0cefbe94625b9935b4da0dfe7c82c1f2f8ab6932
Richard Soares <soaresr870@gmail.com>
2022-07-12 10:57:16 -0300

    data annotations configurated, inserted tables and datas Categorias and Lanches

--- ConsumerFoodSRS/Models/Categoria.cs
+++ ConsumerFoodSRS/Models/Categoria.cs
@@ -1,9 +1,19 @@
-﻿namespace ConsumerFoodSRS.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConsumerFoodSRS.Models;
 
 public class Categoria
 {
     public int CategoriaId { get; set; }
+
+    [Required(ErrorMessage = "Informe o nome da categoria")]
+    [StringLength(100, ErrorMessage = "O tamanho máximo é de {1} caracteres")]
+    [Display(Name = "Nome")]
     public string CategoriaNome { get; set; }
+
+    [Required(ErrorMessage = "Informe a descrição da categoria")]
+    [StringLength(200, ErrorMessage = "O tamanho máximo é de {1} caracteres")]
+    [Display(Name = "Descricão")]
     public string Descricao { get; set; }
 
     public List<Lanche> Lanches { get; set; }
--- ConsumerFoodSRS/Models/Lanche.cs
+++ ConsumerFoodSRS/Models/Lanche.cs
@@ -1,15 +1,44 @@
-﻿namespace ConsumerFoodSRS.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ConsumerFoodSRS.Models;
 
 public class Lanche
 {
     public int LancheId { get; set; }
+
+    [Required(ErrorMessage = "Informe o nome do produto")]
+    [StringLength(100, ErrorMessage = "O tamanho máximo é de {1} caracteres")]
+    [Display(Name = "Nome")]
     public string Nome { get; set; }
+
+    [Required(ErrorMessage = "Informe uma descrição para o produto")]
+    [StringLength(150, ErrorMessage = "O tamanho máximo é de {1} caracteres")]
+    [Display(Name = "Descrição")]
     public string DescricaoCurta { get; set; }
+
+    [StringLength(300, ErrorMessage = "O tamanho máximo é de {1} caracteres")]
+    [Display(Name = "Descrição Detalhada")]
     public string DescricaoDetalhada { get; set; }
+
+    [Required(ErrorMessage = "Informe um preço ao produto")]
+    [Column(TypeName = "decimal(10,2)")]
+    [Display(Name = "Preço")]
+    [Range(1,999.99, ErrorMessage = "Preço deve estar entre 1 e 999.99")]
     public decimal Preco { get; set; }
+
+    [StringLength(200, ErrorMessage = "O tamanho máximo é de {1} caracteres")]
+    [Display(Name = "Caminho da Imagem")]
     public string ImagemUrl { get; set; }
+
+    [StringLength(200, ErrorMessage = "O tamanho máximo é de {1} caracteres")]
+    [Display(Name = "Caminho da Thumbnail")]
     public string ImagemThumbnailUrl { get; set; }
+
+    [Display(Name = "Preferido")]
     public bool IsLanchePreferido { get; set; }
+
+    [Display(Name = "Estoque")]
     public bool EmEstoque { get; set; }
 
     public int CategoriaId { get; set; }
