eb914524a57b7d5a7c5755cbfd9cfe35f2a3454f
Richard Soares <soaresr870@gmail.com>
2022-07-14 23:37:51 -0300

    created TagHelper to email and controller e view of Contato

---
+++ ConsumerFoodSRS/Controllers/ContatoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConsumerFoodSRS.Controllers;
+
+public class ContatoController : Controller
+{
+    public IActionResult Contato()
+    {
+        return View();
+    }
+}
