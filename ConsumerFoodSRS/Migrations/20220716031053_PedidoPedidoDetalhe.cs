f28e1f6b7531e4e73df97b22e00c518187935290
Richard Soares <soaresr870@gmail.com>
2022-07-16 00:13:51 -0300

    created domain model and tables of Pedido and PedidoDetalhes

---
+++ ConsumerFoodSRS/Migrations/20220716031053_PedidoPedidoDetalhe.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ConsumerFoodSRS.Migrations
+{
+    public partial class PedidoPedidoDetalhe : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pedidos",
+                columns: table => new
+                {
+                    PedidoId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Sobrenome = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Endereco = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Complemento = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    Cep = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: false),
+                    Estado = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: true),
+                    Cidade = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: true),
+                    Telefone = table.Column<string>(type: "character varying(25)", maxLength: 25, nullable: false),
+                    Email = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    PedidoTotal = table.Column<decimal>(type: "numeric(18,2)", nullable: false),
+                    TotalItensPedido = table.Column<int>(type: "integer", nullable: false),
+                    PedidoEnviado = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    PedidoEntregueEm = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pedidos", x => x.PedidoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PedidoDetalhes",
+                columns: table => new
+                {
+                    PedidoDetalheId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    PedidoId = table.Column<int>(type: "integer", nullable: false),
+                    LancheId = table.Column<int>(type: "integer", nullable: false),
+                    Quantidade = table.Column<int>(type: "integer", nullable: false),
+                    Preco = table.Column<decimal>(type: "numeric(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PedidoDetalhes", x => x.PedidoDetalheId);
+                    table.ForeignKey(
+                        name: "FK_PedidoDetalhes_Lanches_LancheId",
+                        column: x => x.LancheId,
+                        principalTable: "Lanches",
+                        principalColumn: "LancheId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PedidoDetalhes_Pedidos_PedidoId",
+                        column: x => x.PedidoId,
+                        principalTable: "Pedidos",
+                        principalColumn: "PedidoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PedidoDetalhes_LancheId",
+                table: "PedidoDetalhes",
+                column: "LancheId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PedidoDetalhes_PedidoId",
+                table: "PedidoDetalhes",
+                column: "PedidoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PedidoDetalhes");
+
+            migrationBuilder.DropTable(
+                name: "Pedidos");
+        }
+    }
+}
