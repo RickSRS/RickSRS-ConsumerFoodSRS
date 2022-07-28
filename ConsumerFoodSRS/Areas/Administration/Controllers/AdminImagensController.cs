a9dde338b94ad6d510680b0cebfbed89fdc46b71
Richard Soares <soaresr870@gmail.com>
2022-07-27 22:22:11 -0300

    created view and controller to manager images

---
+++ ConsumerFoodSRS/Areas/Administration/Controllers/AdminImagensController.cs
@@ -0,0 +1,25 @@
+﻿using ConsumerFoodSRS.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace ConsumerFoodSRS.Areas.Administration.Controllers;
+
+[Area("Administration")]
+[Authorize(Roles = "Admin")]
+public class AdminImagensController : Controller
+{
+    private readonly ConfigurationImagens _configurationImagens;
+    private readonly IWebHostEnvironment _hostEnvironment;
+
+    public AdminImagensController(IOptions<ConfigurationImagens> configurationImagens, IWebHostEnvironment webHostEnvironment)
+    {
+        _configurationImagens = configurationImagens.Value;
+        _hostEnvironment = webHostEnvironment;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
