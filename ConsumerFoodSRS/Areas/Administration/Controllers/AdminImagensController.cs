d18cff71445fdfeed4bdc27ad4491e3aa01a08bc
Richard Soares <soaresr870@gmail.com>
2022-07-27 22:45:08 -0300

    created configuration and creation in the archive

--- ConsumerFoodSRS/Areas/Administration/Controllers/AdminImagensController.cs
+++ ConsumerFoodSRS/Areas/Administration/Controllers/AdminImagensController.cs
@@ -22,4 +22,42 @@ public class AdminImagensController : Controller
     {
         return View();
     }
+
+    public async Task<IActionResult> UploadFiles(List<IFormFile> files)
+    {
+        if (!files.Any())
+        {
+            ViewData["Erro"] = "Erro: Arquivo(s) não selecionado(s)";
+            return View(ViewData);
+        }
+
+        if(files.Count > 10)
+        {
+            ViewData["Erro"] = "Erro: No maximo 10 arquivos selecionados";
+            return View(ViewData);
+        }
+
+        long size = files.Sum(f => f.Length);
+        var filePathsName = new List<string>();
+        var filePath = Path.Combine(_hostEnvironment.WebRootPath, _configurationImagens.NomePastaImagensProduto);
+
+        foreach (var item in files)
+        {
+            if (item.FileName.ToUpper().Contains(".JPG") || item.FileName.ToUpper().Contains(".GIF") || item.FileName.ToUpper().Contains(".PNG"))
+            {
+                var fileNameWithPath = $"{filePath}\\{item.FileName}";
+                filePathsName.Add(fileNameWithPath);
+
+                using(var stream = new FileStream(fileNameWithPath, FileMode.Create))
+                {
+                    await item.CopyToAsync(stream);
+                }
+            }
+        }
+
+        ViewData["Resultado"] = $"{files.Count} arquivo(s) salvo(s) com sucesso";
+        ViewBag.Arquivos = filePathsName;
+
+        return View(ViewData);  
+    }
 }
